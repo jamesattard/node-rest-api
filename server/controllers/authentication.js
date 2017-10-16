51a8c28c82adfc3a899c8540c92347cbb4df1757
James Attard <info@jamesattard.com>
2017-10-16 23:22:30 +0200

    creating user records

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -6,12 +6,25 @@ exports.signup = function(req, res, next) {
 
   // See if a user with a given email exists
   User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
 
+    // If a user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    // If a user with email does NOT exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    // save to database
+    user.save(function(err){
+      if (err) { return next(err); }
+
+      // Respond to request indicating the user was created
+      res.json(user);
+    }); 
   });
-
-  // If a user with email does exist, return an error
-
-  // Is a user with email does NOT exist, create and save user record
-
-  // Respond to request indicating the user was created
 }
