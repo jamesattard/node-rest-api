2764dd2bca911c0b23b3c65e481988da326a1925
James Attard <info@jamesattard.com>
2017-10-16 23:07:42 +0200

    user search

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,17 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-  res.send({success: 'true'});
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // See if a user with a given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+
+  });
+
+  // If a user with email does exist, return an error
+
+  // Is a user with email does NOT exist, create and save user record
+
+  // Respond to request indicating the user was created
 }
