7445868b4c676546a1275e0e95b0474f1d2ae3d1
James Attard <info@jamesattard.com>
2017-10-16 20:52:50 +0200

    Express Router setup

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router');
 
 // App Setup
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Server Setup
 const port = process.env.PORT || 3090;
