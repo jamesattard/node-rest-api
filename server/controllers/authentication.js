5d85d2e93f7b1aae4dfa9ef2f64866fb1c1f7f82
James Attard <info@jamesattard.com>
2017-10-20 07:08:21 +0200

    bcrypt password

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -4,6 +4,10 @@ exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide email and password.'});
+  }
+
   // See if a user with a given email exists
   User.findOne({ email: email }, function(err, existingUser) {
     if (err) { return next(err); }
@@ -24,7 +28,7 @@ exports.signup = function(req, res, next) {
       if (err) { return next(err); }
 
       // Respond to request indicating the user was created
-      res.json(user);
+      res.json({ success: true });
     }); 
   });
 }
